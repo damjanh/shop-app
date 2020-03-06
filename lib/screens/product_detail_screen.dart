a9538822679272cc9aacef0b41cfc8772e013be8
damjanh <damjan.hudobreznik@gmail.com>
2020-03-06 13:26:56 +0100

    Provide project in products detail screen.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = "/product-detail";
@@ -6,8 +9,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final product = Provider.of<ProductsProvider>(context).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(productId)),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
     );
   }
 }
