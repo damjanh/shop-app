bb10a5eb529905a98dac55d38a9eb0ed7f4d8a6c
damjanh <damjan.hudobreznik@gmail.com>
2020-03-09 09:09:01 +0100

    Implement cart model and provider.

---
+++ lib/providers/cart.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+
+import '../models/cart_item.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      items.update(
+          productId,
+          (value) => CartItem(
+              id: value.id,
+              title: value.title,
+              price: value.price,
+              quantity: value.quantity + 1));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+  }
+}
