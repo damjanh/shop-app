889ce4380f40ee664c39caa18027e8ddaefdae1d
damjanh <damjan.hudobreznik@gmail.com>
2020-03-16 17:00:56 +0100

    Include app drawer on products manage screen.

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/products_provider.dart';
 import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/userProducts';
@@ -22,6 +23,7 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
