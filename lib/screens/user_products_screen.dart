227b5cb95e0ed30c4e6df95d38e21e8d713863f1
damjanh <damjan.hudobreznik@gmail.com>
2020-03-16 16:59:37 +0100

    Implement manage  products screen.

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/userProducts';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              // Navigate
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: productsData.products.length,
+            itemBuilder: (_, index) {
+              final product = productsData.products[index];
+              return UserProductItem(
+                  title: product.title, imageUrl: product.imageUrl);
+            }),
+      ),
+    );
+  }
+}
