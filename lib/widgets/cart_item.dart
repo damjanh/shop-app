33f11572650eebd6a733e371abac343e0638b775
damjanh <damjan.hudobreznik@gmail.com>
2020-03-16 16:41:01 +0100

    Implement alert dialog for cart item dissmiss.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -15,6 +15,29 @@ class CartItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dismissible(
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) {
