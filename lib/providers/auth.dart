673413818bbe39c3842402a7fe536666eb50c7f2
damjanh <damjan.hudobreznik@gmail.com>
2020-06-03 14:43:22 +0200

    Refactor auth provider.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,26 +9,27 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> signUp(String email, String password) async {
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAa0sDY2O6AN-se1imE0FPXKVSLV05kNhs";
-    final response = await http.post(url,
-        body: json.encode(
-          {
-            'email': email,
-            'password': password,
-            'returnSecureToken': true,
-          },
-        ));
-    print(jsonDecode(response.body));
+    const url = "signUp";
+    const key = "AIzaSyAa0sDY2O6AN-se1imE0FPXKVSLV05kNhs";
+    return _authenticate(email, password, url, key);
   }
 
   Future<void> logIn(String email, String password) async {
-    const url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAa0sDY2O6AN-se1imE0FPXKVSLV05kNhs";
-    final response = await http.post(url, body: json.encode({
-      'email': email,
-      'password': password,
-      'returnSecureToken': true,
-    }));
+    const url = "signInWithPassword";
+    const key = "AIzaSyAa0sDY2O6AN-se1imE0FPXKVSLV05kNhs";
+    return _authenticate(email, password, url, key);
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment, String apiKey) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$apiKey";
+    final response = await http.post(url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }));
     print(jsonDecode(response.body));
   }
 }
