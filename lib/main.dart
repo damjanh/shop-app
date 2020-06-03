08783cea84f1b4231bb80c32c09240ca6c6e39a2
damjanh <damjan.hudobreznik@gmail.com>
2020-06-03 14:25:54 +0200

    Basic implementation of auth sign up flow.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/auth.dart';
 
 import 'providers/cart_provider.dart';
 import 'providers/orders.dart';
 import 'providers/products_provider.dart';
-import 'screens/cart_screen.dart';
-import 'screens/product_detail_screen.dart';
-import 'screens/products_overview_screen.dart';
-import 'screens/orders_screen.dart';
-import 'screens/user_products_screen.dart';
-import 'screens/edit_product_screen.dart';
 import 'screens/auth_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/edit_product_screen.dart';
+import 'screens/orders_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider.value(
           value: ProductsProvider(),
         ),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signUp(String email, String password) async {
+    const url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAa0sDY2O6AN-se1imE0FPXKVSLV05kNhs";
+    final response = await http.post(url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ));
+    print(jsonDecode(response.body));
+  }
+}
