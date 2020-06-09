c5c5225dcd01107c84c86b753d9588e91a146cba
damjanh <damjan.hudobreznik@gmail.com>
2020-06-09 11:53:28 +0200

    Use animated builder instead of manually handling animnation.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -110,7 +110,7 @@ class _AuthCardState extends State<AuthCard>
     _heightAnimation = Tween<Size>(
             begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
         .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
-    _heightAnimation.addListener(() => setState(() {}));
+
     super.initState();
   }
 
@@ -206,12 +206,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        width: deviceSize.width * 0.75,
-        constraints:
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          height: _heightAnimation.value.height,
+          width: deviceSize.width * 0.75,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
