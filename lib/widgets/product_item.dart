149bf0bd212f09b8dcb5d1d3e03363edab15ea86
damjanh <damjan.hudobreznik@gmail.com>
2020-06-09 12:27:53 +0200

    Add product items placeholders.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -20,8 +20,9 @@ class ProductItem extends StatelessWidget {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                 arguments: product.id);
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('/assets/images/placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
         ),
